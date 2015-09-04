43a71e74a498babd5a3e5f9400e4fcca5791c3f4
Federico T <ftedinur@itba.edu.ar>
2015-09-03 22:29:39 -0300

    todo funcionando a la perfeccion, no hay ningun problema, no se aceptan cambios gracias

--- q2.py
+++ q2.py
@@ -1,30 +1,38 @@
-import matplotlib.pyplot as mp
 import random, math, statistics
 
 def main():
 	lambd = 0.04
 	arrivals = init_arrivals(lambd)
 	simulate(arrivals, 20, 4)
-	
+
 def simulate(arrivals, mu, sigma):
 	events = [(arr, "arrival") for arr in arrivals]
 
 	exits = [random.normalvariate(mu, sigma) + arrivals[0]]
 	for arrival in arrivals[1:]:
-		exits.append(exits[-1] + random.normalvariate(mu, sigma) + arrival)
+		exits.append(max(exits[-1], arrival) + random.normalvariate(mu, sigma))
 
 	events.extend([(ex, "exit") for ex in exits])
 	events.sort(key=lambda tup: tup[0])
 
 	queue_count = 0
+	queue_sizes = []
 
-	for ev in events:
+	for ev in events[1:]:
 		if ev[1] == "arrival":
 			queue_count += 1
 			print("llego uno: " + str(queue_count) + " time: " + str(ev[0]))
 		else:
-			queue_count -= 1
+			queue_count = queue_count - 1 if queue_count > 0 else 0
 			print("se fue uno: " + str(queue_count) + " time: " + str(ev[0]))
+
+		if ev[0] < (8 * 60):
+			queue_sizes.append(queue_count)
+
+	if events[-1][0] > (8 * 60):
+		print("overtime was: " + str(events[-1][0] - (8*60)))
+
+	print("avg queue len: " + str(sum(queue_sizes) / len(queue_sizes)))
 
 
 def init_arrivals(lambd):
@@ -34,7 +42,7 @@ def init_arrivals(lambd):
 		current = random.expovariate(lambd)
 		accum += current
 		arrivals.append(accum)
-	
+
 	if arrivals[-1] > (8 * 60):
 		arrivals.pop()
 
